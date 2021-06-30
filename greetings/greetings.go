4fd151e18a4faf1a49e93d15f828e6784a0799ce
Dalton Wais <dalton@fallingcolors.com>
2021-06-29 21:20:24 -0600

    expanded greetings exercise

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,37 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	//if no name was given, return an error with a message
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	// return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v.  Welcome!", name)
-	return message
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+// init sets tintial values for variables used in the function
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+//randomForamt returns one of a set of greeting messages.  The returned message is selected at random.
+func randomFormat() string {
+	// a slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you. %v",
+		"Hail, %v and well met!",
+	}
+
+	// return a randomly selected message format by specifying a random index for the slice of formats
+	return formats[rand.Intn(len(formats))]
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
+
 	"example.com/greetings"
 )
 
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message
+	message, err := greetings.Hello("")
+	// if an error was returned, print it to the console and exit the program
+	if err != nil {
+		fmt.Println(err)
+		//log.Fatal(err)
+	} else {
+		fmt.Println(message)
+	}
+
+	// if no error as returned print the returned message to the console
+
 	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
-	fmt.Println(message);
+	new_message, new_err := greetings.Hello("Gladys")
+
+	if new_err != nil {
+		log.Fatal(new_err)
+	}
+	fmt.Println(new_message)
 }
